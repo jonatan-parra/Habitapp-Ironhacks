dd8866b68127b27dc3e02c5bdba6ea1601a76672
jonatan <japarrat@unal.edu.co>
2017-04-05 21:22:13 -0500

    add dataset house and police station

--- js/map.js
+++ js/map.js
@@ -1,6 +1,11 @@
+//  Normalmente, la casilla de verificación permanece marcada al recargar la página en firefox. Con este comando está desmarcado
+$("input:checkbox").prop('checked', false);
+
+
 var map; // Access global
 var initialMarker; 
 var latLngDepartament = {lat: 41.8708, lng: -87.6505};  // Location of the departament
+var selected_house;
 
 // travel mode initial
 var travel_mode;
@@ -23,17 +28,42 @@ var download_fire_station;  // Downloaded dataset (True/False)
 var showing_fire_station;   // Showing or not the fire_station (True/False)
 var markers_fire_station;    // List of fire_station markers
 
-// var dataset fire station farmers markets
+// var dataset farmers markets
 var data_farmer_market;  // JSON farmer_market
 var download_farmer_market;  // Downloaded dataset (True/False)
 var showing_farmer_market;   // Showing or not the farmer_market (True/False)
 var markers_farmer_market;    // List of farmer_market markers
+
+// var dataset libraries
+var data_libraries;  // JSON libraries
+var download_libraries;  // Downloaded dataset (True/False)
+var showing_libraries;   // Showing or not the libraries (True/False)
+var markers_libraries;    // List of libraries markers
+
+// var dataset police_station
+var data_police_station;  // JSON police_station
+var download_police_station;  // Downloaded dataset (True/False)
+var showing_police_station;   // Showing or not the police_station (True/False)
+var markers_police_station;    // List of police_station markers
+
+// var dataset house
+var data_house;  // JSON house
+var download_house;  // Downloaded dataset (True/False)
+var showing_house;   // Showing or not the house (True/False)
+var markers_house;    // List of house markers
+var listener_house;   // listener on click house
+
 
 // URl Datasets
 var url_parks = "https://data.cityofchicago.org/api/views/vcti-mbcd/rows.json?accessType=DOWNLOAD";
 var url_schools = "https://data.cityofchicago.org/api/views/75e5-35kf/rows.json?accessType=DOWNLOAD";
 var url_fire_station = "https://data.cityofchicago.org/api/views/28km-gtjn/rows.json?accessType=DOWNLOAD";
 var url_farmers_markets = "https://data.cityofchicago.org/api/views/x5xx-pszi/rows.json?accessType=DOWNLOAD";
+var url_libraries = "https://data.cityofchicago.org/api/views/x8fc-8rcq/rows.json?accessType=DOWNLOAD";
+var url_police_station = "https://data.cityofchicago.org/api/views/z8bn-74gv/rows.json?accessType=DOWNLOAD";
+
+// Url house
+var url_house = "https://data.cityofchicago.org/api/views/s6ha-ppgi/rows.json?accessType=DOWNLOAD";
 
 function initApp(){
 	// Init sites
@@ -48,6 +78,15 @@ function initApp(){
 
 	download_farmer_market = false;
 	showing_farmer_market = false;
+
+	download_libraries = false;
+	showing_libraries = false;
+
+	download_police_station = false;
+	showing_police_station = false;
+
+	download_house = false;
+	showing_house = false;
 
 	// Init the map
 	initMap();
@@ -86,6 +125,9 @@ function initMap() {
   	setTimeout('clickBounce()',6000);
   	initialMarker.addListener('click', clickBounce);
 
+  	// Load house
+  	loading_site(url_house, "house");
+
   	// Travel mode initial
 	travel_mode = google.maps.TravelMode.BICYCLING;
 
@@ -115,7 +157,7 @@ function show_or_hide_site(site){
   			if (showing_parks == true){
   				showing_parks = false;  // Change state app
   				// remover marks
-			  	for(var i=0; i<data_parks.data.length; i++){ markers_park[i].setMap(null); }
+			  	for ( s in markers_park){ markers_park[s].setMap(null);	}
   			} else{
   				show_parks();
   				showing_parks = true; // Change state app
@@ -131,7 +173,7 @@ function show_or_hide_site(site){
   			if (showing_schools == true){
   				showing_schools = false;  // Change state app
   				// remover marks
-			  	for(var i=0; i<data_schools.data.length; i++){ markers_schools[i].setMap(null); }
+			  	for ( s in markers_schools){ markers_schools[s].setMap(null);	}
   			} else{
   				show_schools();
   				showing_schools = true; // Change state app
@@ -147,7 +189,7 @@ function show_or_hide_site(site){
   			if (showing_fire_station == true){
   				showing_fire_station = false;  // Change state app
   				// remover marks
-			  	for(var i=0; i<data_fire_station.data.length; i++){ markers_fire_station[i].setMap(null); }
+			  	for ( s in markers_fire_station){ markers_fire_station[s].setMap(null);	}
   			} else{
   				show_fire_station();
   				showing_fire_station= true; // Change state app
@@ -155,7 +197,7 @@ function show_or_hide_site(site){
   		}
 	}
 
-	// Load fire station in the map
+	// Load markets in the map
 	if (site == "farmers_markets"){
 		if (download_farmer_market == false){
   			loading_site(url_farmers_markets, site);
@@ -163,8 +205,8 @@ function show_or_hide_site(site){
   			if (showing_farmer_market == true){
   				showing_farmer_market = false;  // Change state app
   				// remover marks
-			  	for(var i=0; i<data_farmer_market.data.length; i++){ 
-			  		if(i!=16) { markers_farmer_market[i].setMap(null); } 
+			  	for ( s in markers_farmer_market){
+			  		if( s!=16 ){ markers_farmer_market[s].setMap(null); }
 			  	}
   			} else{
   				show_farmer_market();
@@ -172,6 +214,39 @@ function show_or_hide_site(site){
   			}
   		}
 	}
+
+	// Load libraries in the map
+	if (site == "libraries"){
+		if (download_libraries == false){
+  			loading_site(url_libraries, site);
+  		} else {
+  			if (showing_libraries == true){
+  				showing_libraries = false;  // Change state app
+  				// remover marks
+			  	for ( s in markers_libraries){ markers_libraries[s].setMap(null);	}
+  			} else{
+  				show_libraries();
+  				showing_libraries = true; // Change state app
+  			}
+  		}
+	}
+
+	// Load police station in the map
+	if (site == "police"){
+		if (download_police_station == false){
+  			loading_site(url_police_station, site);
+  		} else {
+  			if (showing_police_station == true){
+  				showing_police_station = false;  // Change state app
+  				// remover marks
+			  	for ( s in markers_police_station){ markers_police_station[s].setMap(null);	}
+  			} else{
+  				show_police_station();
+  				showing_police_station = true; // Change state app
+  			}
+  		}
+	}
+	//console.log(site);
 }
 
 // https://developers.google.com/maps/documentation/javascript/distancematrix#travel_modes
@@ -269,6 +344,18 @@ function loading_site(url, name_site) {
 	        	data_farmer_market= JSON.parse(text);
 	        	show_farmer_market();  
    	     		download_farmer_market = true;
+	        } else if ( name_site == "libraries"){
+	        	data_libraries= JSON.parse(text);
+	        	show_libraries();  
+   	     		download_libraries = true;
+	        } else if ( name_site == "police"){
+	        	data_police_station = JSON.parse(text);
+	        	show_police_station();  
+   	     		download_police_station = true;
+	        } else if ( name_site == "house"){
+	        	data_house = JSON.parse(text);
+	        	show_house();  
+   	     		download_house = true;
 	        } else {
 	        	console.log("Error en loading site")
 	        }	        
@@ -281,12 +368,17 @@ function show_parks() {
 	latitude_park = 27;//13;
 	longitude_park = 26; //12;
     markers_park = [];  //add markers on the map
+    name_parks = [];  // Name parks 
 
     for(var i=0; i < data_parks.data.length;  i++){
     	var latLng = JSON.parse('{ "lat":'+ data_parks.data[i][latitude_park] +', "lng":'+ data_parks.data[i][longitude_park] +' }');
-    	markers_park[i] = new google.maps.Marker({
-				    	position: latLng, map: map, title: 'Name park', icon: 'img/tree.png'
-				  	});
+      	var name_park = data_parks.data[i][9];
+      	if ( name_parks.indexOf(name_park) < 0 ) {
+      		name_parks.push(name_park);
+	    	markers_park[i] = new google.maps.Marker({
+					    		position: latLng, map: map, title: name_park, icon: 'img/tree.png'
+					  		});
+    	}
     }
     showing_parks = true; // Change state app
  } 
@@ -296,12 +388,17 @@ function show_schools() {
 	latitude_schools = 17;
 	longitude_schools = 18; ;
     markers_schools = [];  //add markers on the map
+    name_schools = []; // Name schools
 
     for(var i=0; i < data_schools.data.length;  i++){
+    	var name_school = data_schools.data[i][9];
     	var latLng = JSON.parse('{ "lat":'+ data_schools.data[i][latitude_schools] +', "lng":'+ data_schools.data[i][longitude_schools] +' }');
-    	markers_schools[i] = new google.maps.Marker({
-				    	position: latLng, map: map, title: 'Name schools', icon: 'img/school.png'
-				  	});
+    	if ( name_schools.indexOf(name_school) < 0 ) {
+			name_schools.push(name_school);
+    		markers_schools[i] = new google.maps.Marker({
+				    				position: latLng, map: map, title: name_school, icon: 'img/school.png'
+				  				});
+    	}
     }
     showing_schools = true; // Change state app
  } 
@@ -310,9 +407,10 @@ function show_schools() {
 function show_fire_station() {
     markers_fire_station = [];  //add markers on the map
     for(var i=0; i < data_fire_station.data.length;  i++){
+    	var name_fire_station = data_fire_station.data[i][8];
     	var latLng = JSON.parse('{ "lat":'+ data_fire_station.data[i][14][1] +', "lng":'+ data_fire_station.data[i][14][2] +' }');
     	markers_fire_station[i] = new google.maps.Marker({
-				    	position: latLng, map: map, title: 'Name fire station', icon: 'img/fire_station.png'
+				    	position: latLng, map: map, title: name_fire_station, icon: 'img/fire_station.png'
 				  	});
     }
     showing_fire_station = true; // Change state app
@@ -325,17 +423,64 @@ function show_farmer_market(){
     markers_farmer_market = [];  //add markers on the map
 
     for(var i=0; i < data_farmer_market.data.length;  i++){
+    	var name_farmer_market = data_farmer_market.data[i][8];
     	var latLng = JSON.parse('{ "lat":'+ data_farmer_market.data[i][latitude] +', "lng":'+ data_farmer_market.data[i][longitude] +' }');
     	
     	if( i != 16){ // In this position do not existe latitude/longitude
 	    	markers_farmer_market[i] = new google.maps.Marker({
-					    	position: latLng, map: map, title: 'Name market', icon: 'img/market.png'
+					    	position: latLng, map: map, title: name_farmer_market, icon: 'img/market.png'
 					  	});
     	}
     }
     showing_farmer_market = true; // Change state app
 }
 
+// show markers libraries
+function show_libraries() {
+    markers_libraries = [];  //add markers on the map
+    for(var i=0; i < data_libraries.data.length;  i++){
+    	var name_librarie = data_libraries.data[i][8];
+    	var latLng = JSON.parse('{ "lat":'+ data_libraries.data[i][18][1] +', "lng":'+ data_libraries.data[i][18][2] +' }');
+    	markers_libraries[i] = new google.maps.Marker({
+				    	position: latLng, map: map, title: name_librarie, icon: 'img/library.png'
+				  	});
+    }
+    showing_fire_station = true; // Change state app
+} 
+
+// show markers police station
+function show_police_station() {
+    markers_police_station = [];  //add markers on the map
+    for(var i=0; i < data_police_station.data.length;  i++){
+    	var address_police_station = data_police_station.data[i][10];
+    	var latLng = JSON.parse('{ "lat":'+ data_police_station.data[i][20] +', "lng":'+ data_police_station.data[i][21] +' }');
+    	markers_police_station[i] = new google.maps.Marker({
+				    	position: latLng, map: map, title: address_police_station, icon: 'img/police.png'
+				  	});
+    }
+    showing_police_station = true; // Change state app
+} 
+
+// show markers house
+function show_house() {
+    markers_house = [];  //add markers on the map
+    for(var i=0; i < data_house.data.length;  i++){
+    	var address_house = data_house.data[i][12];
+    	var latLng = JSON.parse('{ "lat":'+ data_house.data[i][19] +', "lng":'+ data_house.data[i][20] +' }');
+    	if (data_house.data[i][19] != null){
+	    	markers_house[i] = new google.maps.Marker({
+					    	position: latLng, map: map, title: address_house, icon: 'img/home.png'
+					  	});
+	    	markers_house[i].addListener('click', selectHouse);
+	    }
+    }
+    showing_house = true; // Change state app
+} 
+
+function selectHouse(){
+	//console.log(data_house.data[number_house][13]);
+	console.log("Click in house");
+}
 
 
 function setTravelMode(new_mode){
